d11203e41c2e7f682b46616f0b283e8507e60144
Balaji Shetty Pachai <balaji.pachai08@gmail.com>
2024-05-27 12:41:22 +0530

    chore: removes unnecessary contract

--- src/SNS/IPrice.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
-
-interface IPrice {
-    function getPricePerYear(string memory name)
-        external
-        view
-        returns (uint256);
-}
